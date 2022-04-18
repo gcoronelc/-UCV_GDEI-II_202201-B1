7566fddb45924e39d7889d57e4d445f2cc254c35
Gustavo Coronel <gcoronelc@gmail.com>
2022-04-18 15:25:09 -0500

    Semana 03

---
+++ Semana03/Work5.sql
@@ -0,0 +1,41 @@
+drop FUNCTION dbo.fn_ventas
+go
+
+
+CREATE FUNCTION dbo.fn_ventas ( ) 
+RETURNS @tabla TABLE ( 
+	CategoryID int primary key not null, 
+	CategoryName varchar(50) not null, 
+	quantity int default 0,
+	importe numeric(12,2) default 0
+) AS 
+BEGIN 
+
+	INSERT INTO @tabla(CategoryID,CategoryName) 
+	select CategoryID, CategoryName from Categories;
+
+	update @tabla
+	set 
+		quantity = (select sum(od.Quantity) 
+					from [Order Details] od
+					join Products p on od.ProductID = p.ProductID
+					where p.CategoryId=t.CategoryID),
+		importe = (select sum(od.Quantity*od.UnitPrice) 
+					from [Order Details] od
+					join Products p on od.ProductID = p.ProductID
+					where p.CategoryId=t.CategoryID)
+	from @tabla t;
+
+	RETURN; 
+END; 
+GO
+
+
+
+select * from dbo.fn_ventas();
+go
+
+
+select sum(Quantity), sum(Quantity*UnitPrice)
+from [Order Details];
+go
