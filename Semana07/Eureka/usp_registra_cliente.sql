7503c77adb5331f7cd5181fb829ae01b61ab8143
Gustavo Coronel <gcoronelc@gmail.com>
2022-05-16 16:00:39 -0500

    Semana 7

--- Semana07/Eureka/usp_registra_cliente.sql
+++ Semana07/Eureka/usp_registra_cliente.sql
@@ -6,6 +6,12 @@ select * from cliente order by 1 desc;
 select * from contador;
 go
 
+/*
+ * Estado:
+ *    1: Proceso ok
+ *   -1: Error en el proceso
+ *   -2: Error al crear el codigo del cliente.
+*/
 create procedure usp_registra_cliente
 (
 	@p_paterno    varchar (25),
@@ -22,21 +28,55 @@ create procedure usp_registra_cliente
 as
 begin
 	-- Declaracion de variables
-
+	declare @contador int, @tamanio int;
 	-- Validacion
 
 	-- Proceso
+	set @p_estado = 1;
 	BEGIN TRY 
 		-- Inicio de la TX
 		BEGIN TRANSACTION; 
-
-
+		-- Incrementar el contador
+		update contador set int_contitem = int_contitem + 1
+		where vch_conttabla = 'Cliente';
+		if(@@ROWCOUNT <> 1)
+		begin
+			set @p_estado = -2;
+			throw 51000, 'No se puede crear el codigo del cliente.', 1; 
+		end;
+		select @contador=int_contitem, @tamanio=int_contlongitud
+		from Contador
+		where vch_conttabla = 'Cliente';
+		set @p_codigo = RIGHT(concat('00000',@contador),@tamanio);
+		-- Insertar el registro
+		INSERT INTO CLIENTE(chr_cliecodigo,vch_cliepaterno,vch_cliematerno,vch_clienombre,
+		chr_cliedni,vch_clieciudad,vch_cliedireccion,vch_clietelefono,vch_clieemail)
+		VALUES(@p_codigo,@p_paterno,@p_materno,@p_nombre,@p_dni,@p_ciudad,
+		@p_direccion,@p_telefono,@p_email);
 		-- Confirmar la TX
 		COMMIT TRANSACTION; 
 	END TRY 
 	BEGIN CATCH 
 		-- Cancelar la TX
 		ROLLBACK TRANSACTION; 
+		if(@p_estado = 1) set @p_estado = -1;
 	END CATCH;
 end;
+go
+
+-- Prueba 1
+declare @codigo char(5), @estado int;
+exec usp_registra_cliente 'VASQUEZ','VALVERDE','CARLOS','12345678','Lima',
+'Los Olivos','963476892','carlos@gmail.com', @codigo out, @estado out;
+print concat('Estado: ', @estado);
+print concat('Codigo: ', @codigo);
+go
+
+
+-- Prueba 2
+declare @codigo char(5), @estado int;
+exec usp_registra_cliente 'VASQUEZ','VALVERDE',NULL,'12345678','Lima',
+'Los Olivos','963476892','carlos@gmail.com', @codigo out, @estado out;
+print concat('Estado: ', @estado);
+print concat('Codigo: ', @codigo);
 go
