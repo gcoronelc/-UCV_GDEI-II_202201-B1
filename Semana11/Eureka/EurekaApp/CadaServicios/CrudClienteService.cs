2d2095ba63fcbbff228b6c0c41820feef21ac54f
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-13 14:37:30 -0500

    Semana 11

---
+++ Semana11/Eureka/EurekaApp/CadaServicios/CrudClienteService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadaServicios
+{
+    public class CrudClienteService: GeneralService
+    {
+    }
+}
---
+++ Semana11/Eureka/EurekaApp/CadaServicios/GeneralService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CadaServicios
+{
+    public class GeneralService
+    {
+    }
+}
