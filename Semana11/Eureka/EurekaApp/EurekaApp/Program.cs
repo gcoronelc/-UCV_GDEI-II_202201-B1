088b9ad5e022054934b975541dc9fa4b05ff014d
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-13 16:58:28 -0500

    Semana 11

--- Semana11/Eureka/EurekaApp/EurekaApp/Program.cs
+++ Semana11/Eureka/EurekaApp/EurekaApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaModelo;
+using CapaServicios;
 
 namespace EurekaApp
 {
@@ -14,6 +16,30 @@ namespace EurekaApp
         [STAThread]
         static void Main()
         {
+            
+            ClienteModel cliente = new ClienteModel();
+            cliente.paterno = "ABC";
+            cliente.materno = "XYZ";
+            cliente.nombre = "Gustavo";
+            cliente.dni = "12345678";
+            cliente.ciudad = "43568790";
+            cliente.direccion = "Mi casa";
+            cliente.telefono = "995467345";
+
+            CrudClienteService ClienteService = new CrudClienteService();
+            string codigo = ClienteService.registrarCliente(cliente);
+
+            MessageBox.Show("Estado: " + ClienteService.Estado);
+            MessageBox.Show("Codig: " + codigo);
+            
+
+            /*
+            EurekaService service = new EurekaService();
+            service.RegistrarDeposito("001000023",500.0, "0004");
+            MessageBox.Show(service.Mensaje);
+            */
+
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
