4cece78b5b99d33f6273a7a5c7c4dccd5dfb5d5d
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-20 21:56:19 -0500

    Semana 12

---
+++ Semana12/SQLQuery1.sql
@@ -0,0 +1,20 @@
+
+
+select * from MARCA;
+go
+
+
+insert into marca values(1,'abc','perú');
+go
+
+insert into marca values(2,'6A''s','PERU');
+print @@Error
+go
+
+
+insert into marca values(3,'6A''s','PERU');
+print @@Error
+go
+
+
+
